c8296dafc2297b33c48667f1ad44abca623e2b1a
Brandon Ros <brandonros1@gmail.com>
2020-08-12 16:11:16 -0400

    loading indicator

--- static/frontend.js
+++ static/frontend.js
@@ -3,7 +3,17 @@ import * as Preact from 'https://unpkg.com/preact@10.4.7/dist/preact.module.js'
 import htm from 'https://unpkg.com/htm@3.0.4/dist/htm.module.js'
 // 2. make htm import work with Preact import
 const html = htm.bind(Preact.createElement)
-// 3. define App component
+// 3. define LoadingIndicator component
+class LoadingIndicator extends Preact.Component {
+  constructor() {
+    super()
+  }
+  render() {
+    const { loading } = this.props
+    return html`<p>Loading: ${loading.toString()}</p>`
+  }
+}
+// 4. define App component
 class App extends Preact.Component {
   constructor() {
     super()
@@ -12,14 +22,12 @@ class App extends Preact.Component {
       apartments: []
     }
   }
-  async getApartments() {
-    const response = await fetch('/apartments')
+  async apiRequest(url) {
+    const response = await window.fetch(url)
     if (response.status !== 200) {
       throw new Error('Failed to fetch')
     }
-    const responseBody = await response.json()
-    const apartments = responseBody
-    return apartments
+    return response.json()
   }
   async componentDidMount() {
     try {
@@ -28,7 +36,7 @@ class App extends Preact.Component {
         loading: true
       })
       // fetch apartments
-      const apartments = await this.getApartments()
+      const apartments = await this.apiRequest('/apartments')
       // set state
       this.setState({
         apartments
@@ -46,15 +54,13 @@ class App extends Preact.Component {
   render() {
     const { loading, apartments } = this.state
     return html`
-      <div class="app">
-        <h1>Apartments</h1>
-        <p>loading: ${this.state.loading.toString()}</p>
-        <ul>
-          ${apartments.map(apartment => html`<li key="${apartment.id}">${apartment.name}</li>`)}
-        </ul>
-      </div>
+      <h1>Apartments</h1>
+      <${LoadingIndicator} loading="${loading}"/>
+      <ul>
+        ${apartments.map(apartment => html`<li key="${apartment.id}">${apartment.name}</li>`)}
+      </ul>
     `
   }
 }
 // 4. append rendered App component to node document.body
-Preact.render(html`<${App}></App>`, document.body)
+Preact.render(html`<${App}/>`, document.body)
