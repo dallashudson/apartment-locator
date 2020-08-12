6af3ae8333f3e09de5eaa9be690e473864f05198
Brandon Ros <brandonros1@gmail.com>
2020-08-12 14:56:57 -0400

    get apartments

--- static/frontend.js
+++ static/frontend.js
@@ -3,33 +3,13 @@ import * as Preact from 'https://unpkg.com/preact@10.4.7/dist/preact.module.js'
 import htm from 'https://unpkg.com/htm@3.0.4/dist/htm.module.js'
 // 2. make htm import work with Preact import
 const html = htm.bind(Preact.createElement)
-// 3. define Header component
-class Header extends Preact.Component {
-  render() {
-    const { name } = this.props
-    return html`<h1>${name} List</h1>`
-  }
-}
-// 4. define Footer component
-class Footer extends Preact.Component {
-  render() {
-    const { children } = this.props
-    return html`<footer>${children}</footer>`
-  }
-}
-// 5. define App component
+// 3. define App component
 class App extends Preact.Component {
   constructor() {
     super()
     this.state = {
-      todos: []
+      apartments: []
     }
-  }
-  addTodo() {
-    const { todos } = this.state
-    this.setState({
-      todos: todos.concat(`Item ${todos.length}`)
-    })
   }
   async getApartments() {
     const response = await fetch('/apartments')
@@ -38,23 +18,24 @@ class App extends Preact.Component {
     }
     const responseBody = await response.json()
     const apartments = responseBody
-    console.log(apartments)
+    return apartments
+  }
+  async componentDidMount() {
+    this.setState({
+      apartments: await this.getApartments()
+    })
   }
   render() {
-    const { page } = this.props
-    const { todos } = this.state
+    const { apartments } = this.state
     return html`
       <div class="app">
-        <${Header} name="ToDo's (${page})"></Header>
+        <h1>Apartments</h1>
         <ul>
-          ${todos.map(todo => html`<li key="${todo}">${todo}</li>`)}
+          ${apartments.map(apartment => html`<li key="${apartment.id}">${apartment.name}</li>`)}
         </ul>
-        <button onClick=${() => this.addTodo()}>Add Todo</button>
-        <button onClick=${() => this.getApartments()}>Get Apartments</button>
-        <${Footer}>footer content here</Footer>
       </div>
     `
   }
 }
 // 6. append rendered App component to node document.body
-Preact.render(html`<${App} page="All"></App>`, document.body)
+Preact.render(html`<${App}></App>`, document.body)
